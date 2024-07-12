926516b34d59801541957941b78b2f79b9dddb7a
Rodrigo <rodrigobdiass@gmail.com>
2024-07-12 19:08:37 -0300

    Teste de push

--- tests/test_funcao_ola_turma.py
+++ tests/test_funcao_ola_turma.py
@@ -2,5 +2,5 @@ from app.funcao import funcao_ola_turma
 
 def test_funcao_ola_turma_retorna_ola_jornada():
     saida = funcao_ola_turma()
-    gabarito = "ola jornada"
+    gabarito = "ola jornada e s"
     assert saida == gabarito
