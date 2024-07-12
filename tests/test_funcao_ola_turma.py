468eebd2c69197d548d23b94a300817a621d0a1a
Rodrigo <rodrigobdiass@gmail.com>
2024-07-12 19:11:41 -0300

    teste relacionado ao CI versao do pyenv

--- tests/test_funcao_ola_turma.py
+++ tests/test_funcao_ola_turma.py
@@ -2,5 +2,5 @@ from app.funcao import funcao_ola_turma
 
 def test_funcao_ola_turma_retorna_ola_jornada():
     saida = funcao_ola_turma()
-    gabarito = "ola jornada e s"
+    gabarito = "ola jornada"
     assert saida == gabarito
